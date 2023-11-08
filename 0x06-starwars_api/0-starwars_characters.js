52c9115c1f41a3d55a35a85d7faac900d7e2527c
R-Owino <sayeakinyi@gmail.com>
2023-11-08 12:41:27 +0300

    Get character names from Starwars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,22 +5,47 @@
  */
 
 const request = require('request');
+const movieId = process.argv[2];
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+if (!movieId || isNaN(movieId)) {
+  process.exit(1);
+}
 
-request(url, function (error, response, body) {
+const url = `https://swapi-api.alx-tools.com/films/${movieId}`;
+
+request(url, (error, res, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
+    return;
+  }
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  const fetchCharacterInfo = (characterUrl) => {
+    return new Promise((resolve, reject) => {
+      request(characterUrl, (error, response, characterBody) => {
         if (error) {
-          console.log(error);
+          reject(error);
         } else {
-          console.log(JSON.parse(body).name);
+          const characterData = JSON.parse(characterBody);
+          resolve(characterData.name);
         }
       });
-    }
-  }
+    });
+  };
+
+  const characterPromises = characters.map((character) => {
+    return fetchCharacterInfo(character);
+  });
+
+  Promise.all(characterPromises)
+    .then((characterNames) => {
+      characterNames.forEach((name, index) => {
+        console.log(`${index + 1}. ${name}`);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
