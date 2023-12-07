6925aa52530b5b4b68fe08c21e17a791f93d6b44
R-Owino <sayeakinyi@gmail.com>
2023-12-07 10:46:43 +0300

    refactor: check some tests

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,55 +15,27 @@ def isWinner(x, nums):
     Returns:
         str: name of the winner
     """
-    # check if a number is a prime
-    def isPrime(n):
-        """
-        Checks if a number is a prime
-
-        Args:
-            n (int): number to check
-
-        Returns:
-            bool: True if n is a prime, False otherwise
-        """
+    def get_prime_count(n):
         if n < 2:
-            return False
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    # count prime numbers upto n
-    def countPrimes(n):
-        """
-        Counts the number of prime numbers up to n
-
-        Args:
-            n (int): the upper limit
-
-        Returns:
-            int: the count of prime numbers
-        """
+            return 0
         count = 0
-        for num in range(2, n + 1):
-            if isPrime(num):
+        for i in range(2, n + 1):
+            if n % i == 0:
                 count += 1
         return count
 
-    # play the game for each round and count wins
-    maria_wins = 0
-    ben_wins = 0
-    for n in nums:
-        primes = countPrimes(n)
-        if primes % 2 == 0:
-            ben_wins += 1
+    if not nums or x < 1:
+        return None
+    Maria = 0
+    Ben = 0
+    for i in range(x):
+        if i % 2 == 0:
+            Maria += get_prime_count(nums[i])
         else:
-            maria_wins += 1
-
-    # determine overall winner
-    if maria_wins > ben_wins:
-        return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
+            Ben += get_prime_count(nums[i])
+    if Maria > Ben:
+        return "Maria"
+    elif Ben > Maria:
+        return "Ben"
     else:
         return None
