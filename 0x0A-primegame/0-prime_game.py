29681b95c25e8e78acccc5d2a2e57955075ecbdf
R-Owino <sayeakinyi@gmail.com>
2023-12-07 10:57:24 +0300

    refactor: check on edge cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -15,27 +15,43 @@ def isWinner(x, nums):
     Returns:
         str: name of the winner
     """
-    def get_prime_count(n):
-        if n < 2:
-            return 0
-        count = 0
-        for i in range(2, n + 1):
-            if n % i == 0:
-                count += 1
-        return count
+    # count prime numbers upto n
+    def countPrimes(n):
+        """
+        Counts the number of prime numbers up to n
 
+        Args:
+            n (int): the upper limit
+
+        Returns:
+            int: the count of prime numbers
+        """
+        sieve = [True] * (n+1)
+        sieve[0:2] = [False, False]  # 0 and 1 are not prime
+        for current in range(2, int(n**0.5) + 1):
+            if sieve[current]:  # current is prime
+                for multiple in range(current*current, n+1, current):
+                    sieve[multiple] = False
+        return sum(sieve)
+
+    # Check for edge cases
     if not nums or x < 1:
         return None
-    Maria = 0
-    Ben = 0
+
+    # Play the game for each round and count wins
+    maria_wins = 0
+    ben_wins = 0
     for i in range(x):
-        if i % 2 == 0:
-            Maria += get_prime_count(nums[i])
+        primes = countPrimes(nums[i])
+        if primes % 2 == 0:
+            ben_wins += 1
         else:
-            Ben += get_prime_count(nums[i])
-    if Maria > Ben:
-        return "Maria"
-    elif Ben > Maria:
-        return "Ben"
+            maria_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return 'Maria'
+    elif ben_wins > maria_wins:
+        return 'Ben'
     else:
         return None
